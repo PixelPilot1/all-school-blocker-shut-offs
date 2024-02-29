b12b7eb89897a2801eb30839cd6c722c35b68afd
Pixel Pilot <161532452+PixelPilot1@users.noreply.github.com>
2024-02-28 18:50:54 -0600

    Create links.js

---
+++ links.js
@@ -0,0 +1,49 @@
+Template URL:
+chrome-extension://extension_id_here_please/manifest.json
+
+Extension ID’s:
+GoGuardian: haldlgldplgnggkjaafhelgiaglafanh
+chrome-extension://haldlgldplgnggkjaafhelgiaglafanh/manifest.json
+
+Securly: joflmkccibkooplaeoinecjbmdebglab
+chrome-extension://joflmkccibkooplaeoinecjbmdebglab/manifest.json
+
+Blocksi: pgmjaihnmedpcdkjcgigocogcbffgkbn
+chrome-extension://pgmjaihnmedpcdkjcgigocogcbffgkbn/manifest.json
+
+iBoss: kmffehbidlalibfeklaefnckpidbodff
+chrome-extension://kmffehbidlalibfeklaefnckpidbodff/manifest.json
+
+Fortiguard: igbgpehnbmhgdgjbhkkpedommgmfbeao
+chrome-extension://igbgpehnbmhgdgjbhkkpedommgmfbeao/manifest.json
+
+Cisco Umbrella: jcdhmojfecjfmbdpchihbeilohgnbdci
+chrome-extension://jcdhmojfecjfmbdpchihbeilohgnbdci/manifest.json
+
+NetRef: khfdeghnhlpdfeenmdofgcbilkngngcp
+chrome-extension://khfdeghnhlpdfeenmdofgcbilkngngcp/manifest.json
+
+ContentKeeper: jdogphakondfdmcanpapfahkdomaicfa
+chrome-extension://jdogphakondfdmcanpapfahkdomaicfa/manifest.json
+
+Hapara: kbohafcopfpigkjdimdcdgenlhkmhbnc
+chrome-extension://kbohafcopfpigkjdimdcdgenlhkmhbnc/manifest.json
+
+Smoothwall: jbldkhfglmgeihlcaeliadhipokhocnm
+chrome-extension://jbldkhfglmgeihlcaeliadhipokhocnm/manifest.json
+
+Linewize: ddfbkhpmcdbciejenfcolaaiebnjcbfc
+chrome-extension://ddfbkhpmcdbciejenfcolaaiebnjcbfc/manifest.json
+
+LANSchool: baleiojnjpgeojohhhfbichcodgljmnj 
+chrome-extension://baleiojnjpgeojohhhfbichcodgljmnj/manifest.json
+
+If your blocker is not on this list: go to chrome://extensions, details on your blockers extension, then copy the code in the url (after chrome://extensions/?id=).
+
+B: chrome://kill
+B2: chrome://crash (another way for chrome://kill)
+C: chrome://hang
+
+
+watch the video and like the goguirdian bookmark just use the .json text from whatever blocker you use.
+  https://youtu.be/797v9IoCvis?si=dTASuehd96Pp5LOV
